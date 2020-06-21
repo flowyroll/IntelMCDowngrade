b6638c7a9a5a37453d84857f353ba953090c4d84
Daniel <daniel.mogimi@gmail.com>
2020-06-21 12:20:14 -0400

    back up existing MCs

--- install_mc.sh
+++ install_mc.sh
@@ -1,3 +1,4 @@
+sudo cp  /lib/firmware/intel-ucode/ /lib/firmware/intel-ucode_backup -r # Backup existing MC (just in case!)
 sudo rm -rf /lib/firmware/intel-ucode/*
 sudo cp ./Intel/$1 /lib/firmware/intel-ucode/
 echo 1 > /sys/devices/system/cpu/microcode/reload
